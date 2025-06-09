b253ac54a14ead26558fd48666d62e51633c5d75
Nagaraj <nagarajuande255@gmail.com>
2025-06-10 00:32:40 +0530

    terraform

---
+++ condition/ec2.tf
@@ -0,0 +1,29 @@
+# This script will run instance & security Group inbound rules & ourbound rules 
+resource "aws_instance" "roboshop" {
+  ami           = var.ami_id  # left & right side no need to same right side we can change (left side is syntax to need to change)
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+  
+  tags = var.ec2_tags
+}
+resource "aws_security_group" "allow_all" {
+    name        = var.sg_name
+    description = var.sg_description
+
+    ingress {
+        from_port        = var.from_port
+        to_port          = var.to_port
+        protocol         = "-1"
+        cidr_blocks      = var.cidr_blocks
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    egress {
+        from_port        = var.from_port
+        to_port          = var.to_port
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = var.sg_tags
+}

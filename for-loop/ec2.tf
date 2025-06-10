4a296797da9afb3398a45a17a689ee2d26344697
Nagaraj <nagarajuande255@gmail.com>
2025-06-10 10:58:30 +0530

    terraform

--- for-loop/ec2.tf
+++ for-loop/ec2.tf
@@ -1,14 +1,11 @@
-# This script will run instance & security Group inbound rules & ourbound rules 
 resource "aws_instance" "roboshop" {
   for_each = var.instances
-  count = 4     # This count = 4 line will create 4 instances at a time using loop concepts
   ami           = var.ami_id  # left & right side no need to same right side we can change (left side is syntax to need to change)
-  instance_type = var.environment == "dev" ? "t3.micro" : "t3.small" # this is the syntax of condition
-  # line 4 command dev is ture it take "t3.micro" & its false it take "t3.small" 
+  instance_type = each.value #  This [each.value] is a ec2 instance type like t3.micro & t3.small etc...
   vpc_security_group_ids = [ aws_security_group.allow_all.id ]
   
   tags = {
-    Name: var.instances[count.index] # This [count.index] will create the instance with diffent name like mongodb, redis etc...
+    Name: each.key # This [each.key] is a instance name like mongodb, redis etc...
   }
 }
 resource "aws_security_group" "allow_all" {

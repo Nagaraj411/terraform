19728e7d3cc04c37faed1c46b357fbf4d5fcb898
Nagaraj <nagarajuande255@gmail.com>
2025-06-10 11:15:14 +0530

    terraform

--- for-loop/r53.tf
+++ for-loop/r53.tf
@@ -1,11 +1,8 @@
-# resource "aws_route53_record" "www" {
-#   count = 4
-#   zone_id = var.zone_id  # zone_id given in variables it will check 
-#   name    = "${var.instances[count.index]}.${var.domain_name}" # it will mongodb.devops84.shop
-#   # variable 1 {var.instances[count.index]}   It will take the instances name like mongodb, catalogue
-#   # variable 2 {var.domain_name}  It will take the domain name & connect to instance like mongodb.devops84.shop & catalogue.devops84.shop etc..
-#   #This is called INTERPOLATION  2 variables along with string
-#   type    = "A"
-#   ttl     = 1
-#   records = [aws_instance.roboshop[count.index].private_ip]
-# }
+resource "aws_route53_record" "www" {
+  for_each = aws_instance.roboshop
+  zone_id = var.zone_id  # zone_id given in variables it will check 
+  name    = "${each.key}.${var.domain_name}" # it will mongodb.devops84.shop
+  type    = "A"
+  ttl     = 1
+  records = [each.value.private_ip]
+}

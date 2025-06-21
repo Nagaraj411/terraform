a7265c2d77e0650e3257f7185b560aeb2113dcd2
Nagaraj <nagarajuande255@gmail.com>
2025-06-21 12:19:00 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -2,10 +2,10 @@
 resource "aws_instance" "roboshop" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+  vpc_security_group_ids = [ aws_security_group.allow_all.id ] # Security Group ID
   
-  tags = {
-    Name = "Terraform main"
+  tags = { # Tags for the instance
+    Name = "Terraform main" # Name of the instance
   }
 }
 resource "aws_security_group" "allow_all" {
